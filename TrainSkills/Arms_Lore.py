24228cb04801d4fba04128398bd39f7605ff1e3c
Simone Caporale <caporalesimone@gmail.com>
2024-07-07 15:21:11 +0200

    Fix Arms Lore

--- TrainSkills/Arms_Lore.py
+++ TrainSkills/Arms_Lore.py
@@ -1,11 +1,10 @@
-pause = 1 # N * 1 second
 target = Target.PromptTarget("Target a weapon",20)
 
 while True:
     Player.UseSkill("Arms Lore")
     Target.WaitForTarget(10000, False)
     Target.TargetExecute(target)
-    for x in reversed(range(pause)):
-        #Player.HeadMessage(20, x)
-        Misc.Pause(1000)
+    Misc.Pause(1000)
+    if Player.GetSkillValue("Arms Lore") >= 100:
+        break
 
